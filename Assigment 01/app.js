e98e2abbcdbe935f4d3b966e74f924cca7dfc64a
sameer <sameersultan1001@gmail.com>
2023-10-16 16:00:02 +0500

    Assignment 01

---
+++ Assigment 01/app.js
@@ -0,0 +1,26 @@
+// Question /Answer ============> 01
+// Get the visitor's name (you can customize this part)
+var visitorName = prompt("Hello there! What's your name?");
+
+// Display a personalized greeting
+alert("Hello, " + visitorName + "! Welcome to our website!");
+
+// Question /Answer ============> 02
+alert("Please enter a valid password");
+
+// Question /Answer ============> 03
+// Display a message with line breaks
+ "Hello,\nWelcome to JS Land !\Happy Coding !.";
+alert(message);
+
+// Question /Answer ============> 03
+// Check if the checkbox is checked
+ confirm("Happy coding","Please check the checkbox to prevent the dialog box.");
+
+// Question /Answer ============> 04
+alert('hello . I can run JS through my web browser console')
+console.log(alert('hello . I can run JS through my web browser console'))
+
+
+
+
